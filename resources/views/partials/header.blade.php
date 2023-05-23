6a7497027f2e6a2ef3f68df454c8ff2ea1fc9039
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-23 16:45:58 +0200

    Costum header links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,16 @@
 <header>
-    <h1 class="text-danger text-uppercase px-5">netflix</h1>
-    <ul class="fw-semibold">
-        <li><a href="{{ route('home') }}" class="{{ Request::is('/') ? 'active' : '' }}">home</a></li>
-        <li><a href="{{ route('movies.index') }}" class="{{ Request::is('movies') ? 'active' : '' }}">movies</a></li>
-    </ul>
+    <div class="row container">
+        <div class="col-6">
+            <h1 class="text-danger text-uppercase">netflix</h1>
+
+        </div>
+        <div class="col-6">
+            <ul class="fw-semibold">
+                <li><a href="{{ route('home') }}" class="{{ Request::is('/') ? 'active' : '' }}">home</a></li>
+                <li><a href="{{ route('movies.index') }}" class="{{ Request::is('movies') ? 'active' : '' }}">movies</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
 </header>
