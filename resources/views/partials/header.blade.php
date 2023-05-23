b8d32b1820a7ed7a2c5e81dab11ff47a24564260
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-23 16:25:34 +0200

    Added class active

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <h1 class="text-danger text-uppercase px-5">netflix</h1>
     <ul class="fw-semibold">
-        <li><a href="{{ route('home') }}">home</a></li>
-        <li><a href="{{ route('movies.index') }}">movies</a></li>
+        <li><a href="{{ route('home') }}" class="{{ Request::is('/') ? 'active' : '' }}">home</a></li>
+        <li><a href="{{ route('movies.index') }}" class="{{ Request::is('movies') ? 'active' : '' }}">movies</a></li>
     </ul>
 </header>
