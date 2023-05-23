bf2831895a40387a53a90549019e70115618235c
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-23 16:31:49 +0200

    Added class link-movie for transition style

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+    <h3 class="text-danger text-uppercase mb-5">movies</h3>
     <ul>
         @foreach ($movies as $movie)
             <li>
-                <a href="{{ route('movies.show', ['id' => $movie->id]) }}">{{ $movie->title }}</a>
+                <a href="{{ route('movies.show', ['id' => $movie->id]) }}"
+                    class="link-movie fw-semibold">{{ $movie->title }}</a>
             </li>
         @endforeach
     </ul>
